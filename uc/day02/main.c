bf3d115ff69e9e6ae362223713f3201d69d8b3aa
youname <you@example.com>
2019-06-26 22:32:10 +0800

    uninx c语言

---
+++ uc/day02/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <p_math.h>
+
+int main(void){
+    int valA=6,valB=2;
+    printf("%d+%d=%d\n",valA,valB,p_add(valA,valB));
+    //printf("%d*%d=%d\n",valA,valB,p_mul(valA,valB));
+    return 0;
+}
